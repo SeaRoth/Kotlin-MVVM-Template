69872e8b23d745865495dd5f0c6825f0987f60d6
The SeaRoth <coryr32@gmail.com>
2019-05-29 17:13:53 -0700

    added google maps integration for weather activity

--- app/src/main/java/com/searoth/template/infrastructure/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/searoth/template/infrastructure/ui/weather/WeatherActivity.kt
@@ -7,6 +7,12 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.searoth.template.R
 import com.searoth.template.databinding.ActivityWeatherBinding
 import com.searoth.template.infrastructure.ui.base.BaseActivity
@@ -15,12 +21,14 @@ import kotlinx.android.synthetic.main.bottom_nav.*
 import kotlinx.android.synthetic.main.include_navigation_drawer.*
 import timber.log.Timber
 
-class WeatherActivity : BaseActivity() {
+class WeatherActivity : BaseActivity() , OnMapReadyCallback {
     private val weatherActivityViewModel: WeatherActivityViewModel by lazy {
         ViewModelProviders.of(this).get(WeatherActivityViewModel::class.java)
     }
 
+    private lateinit var mMap: GoogleMap
     lateinit var layoutBinding: ActivityWeatherBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         layoutBinding = DataBindingUtil.setContentView<ActivityWeatherBinding>(this, R.layout.activity_weather).apply {
@@ -31,6 +39,28 @@ class WeatherActivity : BaseActivity() {
         setSupportActionBar(toolbar)
         setupHomeAsUp(ActionBarStyle.NAV_BUTTON, statusColor = R.color.tenx_red)
         supportActionBar?.setTitle("")
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     override fun inflateOptionsMenu(menu: Menu){
